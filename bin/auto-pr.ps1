ae59d39ca9a3d23dcacbab634463bbb2a20f4708
Lyric Allen <186507173+eclecticbouquet@users.noreply.github.com>
2025-01-31 12:41:49 -0600

    Fix line endings

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,1 @@
-param(
-    # overwrite upstream param
-    [String]$upstream = "eclecticbouquet/pail:master"
-)
-
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
-$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$PSScriptRoot/../bucket" # checks the parent dir
-& $autopr -Dir $dir -Upstream $Upstream @Args
+param(    # overwrite upstream param    [String]$upstream = "eclecticbouquet/pail:master")if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"$dir = "$PSScriptRoot/../bucket" # checks the parent dir& $autopr -Dir $dir -Upstream $Upstream @Args
--- bin/formatjson.ps1
+++ bin/formatjson.ps1
@@ -1,4 +1,1 @@
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
-$formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
-$path = "$PSScriptRoot/../bucket" # checks the parent dir
-& $formatjson -Dir $path @Args
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }$formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"$path = "$PSScriptRoot/../bucket" # checks the parent dir& $formatjson -Dir $path @Args
